63856a737893249258dfe257265bfb883935fc2f
muhamadzaimzamzami <mzaimzamzami92@gmail.com>
2023-01-24 00:24:47 +0700

    Update hari kedua

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flask.json import JSONEncoder
+from datetime import datetime
 
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
 app = Flask(__name__)
 CORS(app)
+app.json_encoder = CustomJSONEncoder
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from database import mysql
 from flask import jsonify, json
 from flask import flash, request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, date
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -36,15 +37,30 @@ def register():
             'error': str(e)
         }), 400)
 
-@app.route('/users')
+@app.route('/getUser')
 def users():
     try:
+        idUsers = request.args.get('id')
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users")
-        empRows = cursor.fetchall()
-        respone = json.dumps(empRows)
-        return respone
+        cursor.execute("SELECT id, nama, no_telp, email, password FROM users WHERE id = %s", idUsers)
+        dtUsers = cursor.fetchone()
+        if dtUsers:
+            response = jsonify({
+                'result': {
+                    'id' : str(dtUsers['id']),
+                    'nama' : dtUsers['nama'],
+                    'no_telp' : dtUsers['no_telp'],
+                    'email' : dtUsers['email'],
+                    'password' : dtUsers['password'],
+                }
+            })
+            return response
+        else:
+            return jsonify({
+                'message': 'Data Tidak ditemukan',
+            })
+
     except Exception as e:
         return make_response(jsonify({
             'error': str(e)
@@ -65,7 +81,14 @@ def login():
             response = jsonify({
                 'error': False,
                 'message': 'Login Berhasil',
-                'result': resp
+                'result': {
+                    'id' : str(resp['id']),
+                    'nama' : resp['nama'],
+                    'no_telp' : resp['no_telp'],
+                    'email' : resp['email'],
+                    'password' : resp['password'],
+                    'saldo' :resp['saldo']
+                }
             })
             return response
         else:
@@ -94,7 +117,13 @@ def getHomeInfo():
             response = jsonify({
                 'error': False,
                 'message': 'Success',
-                'result': resp
+                'result': {
+                    'id': str(resp['id']),
+                    'nama': resp['nama'],
+                    'saldo': resp['saldo'],
+                    'jml_mobil': str(resp['jml_mobil']),
+                    'jml_motor': str(resp['jml_motor'])
+                }
             })
             return response
         else:
@@ -123,7 +152,16 @@ def getUserVehicle():
             response = jsonify({
                 'error': False,
                 'message': 'Success',
-                'result': resp
+                # `id`, `id_users`, `merek`, `model`, `warna`, `no_polisi`, `jenis`, `created_at`, `updated_at`
+                'result': {
+                    'id': str(resp['id']),
+                    'id_user': str(resp['id_users']),
+                    'merek': resp['merek'],
+                    'model': resp['model'],
+                    'no_polisi': resp['no_polisi'],
+                    'jenis': resp['jenis'],
+                    'warna': resp['warna']
+                }
             })
             return response
         else:
@@ -137,6 +175,115 @@ def getUserVehicle():
             'error': str(e)
         }), 400)  
 
+@app.route('/getParkir')
+def getParkir():
+    try:
+        idUsers = request.args.get('id_user')
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        query = "SELECT * FROM parking WHERE idUser = %s AND (status = 'M' OR status = 'P')"
+        bindData = (idUsers)
+        cursor.execute(query, bindData)
+        resp = cursor.fetchone()
+        # if resp and check_password_hash(resp[2], password):
+        if resp:
+            queryVehicles = "SELECT merek, model, no_polisi, jenis FROM vehicles WHERE id = %s"
+            idVehicle = resp['kendaraan']
+            cursor.execute(queryVehicles, idVehicle)
+            dataVehicle = cursor.fetchone()
+
+            queryVehicles = "SELECT nama, kota, tarif_mobil, tarif_motor FROM parking_place WHERE id = %s"
+            idVehicle = resp['tempat']
+            cursor.execute(queryVehicles, idVehicle)
+            dataPlace = cursor.fetchone()
+
+            response = jsonify({
+                'error': False,
+                'message': 'Success',
+                # `id`, `idUser`, `kendaraan`, `tempat`, `waktuMasuk`, `waktuBayar`, `waktuKeluar`, `biaya`, `status`
+                'result': {
+                    'id' : str(resp['id']),
+                    'id_user' : str(resp['idUser']),
+                    'kendaraan' : dataVehicle,
+                    'tempat' : dataPlace,
+                    'waktu_masuk' : datetime.strptime(str(resp['waktuMasuk']), "%Y-%m-%d %H:%M:%S"),
+                    'waktu_bayar' : resp['waktuBayar'],
+                    'waktu_keluar' : resp['waktuKeluar'],
+                    'biaya' : resp['biaya'],
+                    'status' : resp['status'],
+                }
+            })
+            return response
+        else:
+            return jsonify({
+                'error': True,
+                'message': 'Data Tidak ditemukan',
+                
+            })
+    except Exception as e:
+        return make_response(jsonify({
+            'error': str(e)
+        }), 400)  
+@app.route('/addVehicle', methods=['POST'])
+def addVehicle():
+    try:
+        data = request.form
+        idUser = data['id_user']
+        merek = data['merek']
+        model = data['model']
+        warna = data['warna']
+        no_polis = data['no_polisi']
+        jenis = data['jenis']
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        query = "INSERT INTO vehicles (id_users, merek, model, warna, no_polisi, jenis) VALUES(%s,%s,%s,%s,%s,%s)"
+        bindData = (idUser, merek, model, warna, no_polis, jenis)
+        exe = cursor.execute(query, bindData)
+        conn.commit()
+        if exe:
+            response = jsonify({
+                'error': False,
+                'message': 'Data Kendaraan Berhasil Ditambahkan',
+            })
+            return response
+        else:
+            response = jsonify({
+                'error': True,
+                'message': 'Terjadi Kesalahan',
+            })
+            return response
+    except Exception as e:
+        return make_response(jsonify({
+            'error': str(e)
+        }), 400)
+@app.route('/topUp', methods=['PUT'])
+def toUp():
+    try:
+        data = request.form
+        idUser = data['id']
+        saldo = data['saldo']
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        query = "UPDATE users SET saldo = %s WHERE id = %s"
+        bindData = (saldo, idUser)
+        exe = cursor.execute(query, bindData)
+        conn.commit()
+        if exe:
+            response = jsonify({
+                'error': False,
+                'message': 'Pengisian Saldo Berhasil',
+            })
+            return response
+        else:
+            response = jsonify({
+                'error': True,
+                'message': 'Terjadi Kesalahan',
+            })
+            return response
+    except Exception as e:
+        return make_response(jsonify({
+            'error': str(e)
+        }), 400)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='192.168.1.6')
